a8c37e2baaff297298351971d420f68aec2667ec
Lituta <lituta@outlook.com>
2018-08-30 19:13:16 -0700

    add output jsonhead

--- src/get_jsonhead.py
+++ src/get_jsonhead.py
@@ -1,45 +1,8 @@
-from SPARQLWrapper import SPARQLWrapper
-import json
-import os
-
-
-ENDPOINT = 'http://gaiadev01.isi.edu:3030/rpi0826aif0830/query'
-PREFIX = '''
-PREFIX aida: <https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/InterchangeOntology#>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX xij: <http://isi.edu/xij-rule-set#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX ldcOnt: <https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/SeedlingOntology#>
-'''
-ldcOnt = "https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/SeedlingOntology#"
-ENTITY_TYPE = [
-    ldcOnt + "Organization",
-    ldcOnt + "Person",
-    ldcOnt + "GeopoliticalEntity",
-    ldcOnt + "Location",
-    ldcOnt + "Facility"
-]
-OUTPUT_ENTITY = os.path.join(os.path.dirname(__file__), '../outputs/entity_head.json')
-OUTPUT_EVENT = os.path.join(os.path.dirname(__file__), '../outputs/event_head.json')
-
-sw = SPARQLWrapper(ENDPOINT)
-entity_json = {}
-event_json = {}
-
-
-def dump_file(head_json, out_file):
-    with open(out_file, 'w') as f:
-        json.dump(head_json, f, indent=2)
-
-
-def select(query_str):
-    sw.setQuery(PREFIX + query_str)
-    sw.setReturnFormat('json')
-    return sw.query().convert()['results']['bindings']
+from utils import *
 
 
 def load_entity():
-    print(' - start - load entity')
+    entity_json = {}
     q = '''
     SELECT ?e (MAX(?label) as ?name) ?type ?linkTarget
     WHERE {
@@ -56,11 +19,11 @@ def load_entity():
     '''
     for x in select(q):
         entity_json[x['e']['value']] = [x['name']['value'], x['type']['value'], x['linkTarget']['value']]
-    print(' - done - load entity')
+    return entity_json
 
 
-def load_event():
-    print(' - start - load event')
+def load_event(entity_json=None):
+    event_json = {}
     q = '''
     SELECT distinct ?e ?type ?doc
     WHERE {
@@ -104,19 +67,11 @@ def load_event():
         ''' % evt_uri
         for record in select(q_ent):
             ent = record['ent']['value']
-            ent_type = entity_json[ent][1]
+            ent_type = entity_json[ent][1] if entity_json else select(query_type_sparql(ent))[0]['t']['value']
             event_json[evt_uri][ent_type].append(ent)
         cnt += 1
         if cnt % 200 == 0:
             print('   %d of %d' % (cnt, total))
-    print(' - done - load event')
+    print(' - load event done')
+    return event_json
 
-
-def run():
-    load_entity()
-    dump_file(entity_json, OUTPUT_ENTITY)
-    load_event()
-    dump_file(event_json, OUTPUT_EVENT)
-
-
-run()
