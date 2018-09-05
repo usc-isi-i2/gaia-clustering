0a289ce396601c8529d07ce088676380fd36eb4c
Xi Jin <fate_stigma@hotmail.com>
2018-09-05 12:01:59 -0700

    Refator code for entity cluster

--- multi_layer_network/test/baseline2.exe.py
+++ multi_layer_network/test/baseline2.exe.py
@@ -1,12 +1,14 @@
 import sys
+
 sys.path.append("..")
 import src.event_baseline2 as eb2
+
 edgelist = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/entity.edgelist"
 path_to_cluster_heads = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/entity.json"
-path_to_new_cluster_head ="/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/entity2.json"
+path_to_new_cluster_head = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/entity2.json"
 input_file = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/event.json"
 path_to_output = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/event.jl"
 
-entity2cluster = eb2.get_resolved_entity(edgelist,path_to_cluster_heads,path_to_new_cluster_head)
-print len(entity2cluster)
-eb2.event_baseline_linking(input_file, path_to_output,entity2cluster)
+entity2cluster = eb2.get_resolved_entity(edgelist, path_to_cluster_heads, path_to_new_cluster_head)
+print(len(entity2cluster))
+eb2.event_baseline_linking(input_file, path_to_output, entity2cluster)
--- multi_layer_network/test/from_jsonhead2cluster.py
+++ multi_layer_network/test/from_jsonhead2cluster.py
@@ -1,37 +1,35 @@
-import sys
-sys.path.append("..")
-import src.minhash2 as lel
+import os
+import multi_layer_network.src.minhash2 as lel
 import networkx as nx
 import json
-import codecs
 from ast import literal_eval
 import time
 
 start = time.clock()
 
-
-intput_file = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/entity.json"
-output_file = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/entity.edgelist"
+output_dir = os.path.join(os.path.dirname(__file__), '../../outputs')
+intput_file = output_dir + '/entity.json'
+output_file = output_dir + 'entity.edgelist'
 
 lel.get_links_edge_list(intput_file, output_file)
 G = nx.Graph()
 path_to_cluster_heads = intput_file
 edgelist = output_file
-outputfile = "/Users/xinhuang/Documents/isi/clustering/gaia-clustering/outputs/entity.jl"
+outputfile = output_dir + '/entity.jl'
 
-with open(edgelist,"r") as edges:
+with open(edgelist, "r") as edges:
     G.add_nodes_from(literal_eval(edges.readline()))
     for edge in edges:
         edge_nodes = literal_eval(edge)
         G.add_edge(edge_nodes[0], edge_nodes[1])
 
 cc = nx.connected_components(G)
-cluster_heads = json.load(open(path_to_cluster_heads),encoding="utf-8")
-with codecs.open(outputfile+"_with_attr", 'w',encoding="utf-8") as output:
+cluster_heads = json.load(open(path_to_cluster_heads))
+with open(outputfile + '_with_attr', 'w') as output:
     for c in cc:
         answer = dict()
-        answer['entities'] = map(lambda x:cluster_heads[x],list(c))
-        json.dump(answer, output, encoding="utf-8", ensure_ascii=False)
+        answer['entities'] = [cluster_heads[x] for x in c]
+        json.dump(answer, output)
         output.write('\n')
 
 cc = nx.connected_components(G)
@@ -39,8 +37,8 @@ with open(outputfile, 'w') as output:
     for c in cc:
         answer = dict()
         answer['entities'] = list(c)
-        json.dump(answer, output,encoding="utf-8",ensure_ascii=False)
+        json.dump(answer, output)
         output.write('\n')
 
 elapsed = (time.clock() - start)
-print("Time used:",elapsed)
+print("Time used:", elapsed)
